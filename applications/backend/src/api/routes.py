a0407ff9a477ffc8d30a50178075c15dcb2a56b7
miulab7 <miulab7@gmail.com>
2025-03-24 18:10:59 +0900

    🔧 Change output format

--- applications/backend/src/api/routes.py
+++ applications/backend/src/api/routes.py
@@ -49,7 +49,8 @@ async def generate_questions(request: QuestionGenerationRequest) -> StreamingRes
     chat_service: ChatService = ChatService(prompt_directory=pathlib.Path("data"))
     return StreamingResponse(
         chat_service.generate_questions_stream(question_input),
-        media_type="text/event-stream",
+        # media_type="text/event-stream",
+        media_type="application/json",
     )
 
 
@@ -75,5 +76,6 @@ async def generate_reply(request: ReplyGenerationRequest) -> StreamingResponse:
     chat_service: ChatService = ChatService(prompt_directory=pathlib.Path("data"))
     return StreamingResponse(
         chat_service.generate_reply_stream(reply_input),
-        media_type="text/event-stream",
+        # media_type="text/event-stream",
+        media_type="application/json",
     )
